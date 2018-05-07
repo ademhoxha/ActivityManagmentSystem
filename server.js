0bbe12d7a5ae1ca6a74799c2a34378e78f325881
LAPTOP-6KJE2SEG\ademh <adem.hoxha@hotmail.it>
2018-05-07 23:12:04 +0200

    angular added

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var app = express()
 app.use(bodyParser.json());
 
 
-app.all("*", (req,res) => {
+/*app.all("/api", (req,res) => {
     res.send("TRY...")
-})
+})*/
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.listen(process.env.PORT || 8080)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './contacts/hello/hello.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
