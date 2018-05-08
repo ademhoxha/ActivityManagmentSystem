b8564639df14bc401b19edf0b9948dd042b9fa57
AzureAD\AdemHoxha <adem.hoxha@hotmail.it>
2018-05-08 12:33:04 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require("body-parser");
+const path = require('path');
 
 var app = express()
 app.use(bodyParser.json());
@@ -9,7 +10,10 @@ app.all("/api", (req,res) => {
     res.status(200).json({ok:"JSON INVIATO"});
 })
 
-var distDir = __dirname + "./../dist/";
-app.use(express.static(distDir));
+/*var distDir = __dirname + "./../dist/";
+app.use(express.static(distDir));*/
+app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './../dist/index.html'));
+})
 
 app.listen(process.env.PORT || 8080)
